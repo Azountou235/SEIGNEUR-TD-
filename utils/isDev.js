2e84f70ba7ca17200335c42299bf52a7c3a45bc5
Azountou235 <Azountouleseigneur@gmail.com>
2026-08-03 17:43:45 +0100

    Add isDev function to check developer numbers

---
+++ utils/isDev.js
@@ -0,0 +1,9 @@
+const DEV_NUMBERS = ['23591234567', '23591234568'];
+
+function isDev(msg) {
+  const senderJid = msg.key.participantPn || msg.key.participantAlt || msg.key.participant || msg.key.remoteJidAlt || msg.key.remoteJid;
+  const senderNumber = senderJid.split('@')[0].split(':')[0];
+  return DEV_NUMBERS.includes(senderNumber);
+}
+
+module.exports = { isDev };
