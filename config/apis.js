a5b8b673ecd822e1412153d225b008335c43b9a0
Azountou235 <Azountouleseigneur@gmail.com>
2026-08-03 17:53:43 +0100

    Create apis.js

---
+++ config/apis.js
@@ -0,0 +1,4 @@
+module.exports = {
+  KEITH_BASE: process.env.KEITH_BASE || 'https://apiskeith2-production-ec66.up.railway.app',
+  NEXRAY_BASE: process.env.NEXRAY_BASE || 'https://api.nexray.eu.cc',
+};
