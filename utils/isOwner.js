bcff6e6c79abe91e1c99dfecbc5a3023be99b1d1
Azountou235 <Azountouleseigneur@gmail.com>
2026-08-04 00:11:13 +0100

    Update isOwner function to use settingsStore
    Refactor isOwner function to retrieve owner number from settingsStore.

--- utils/isOwner.js
+++ utils/isOwner.js
@@ -1,12 +1,14 @@
 const config = require('../config/config');
+const settingsStore = require('./settingsStore');
 
 function isOwner(msg) {
   if (msg.key.fromMe) return true;
 
   const senderJid = msg.key.participantPn || msg.key.participantAlt || msg.key.participant || msg.key.remoteJidAlt || msg.key.remoteJid;
-const senderNumber = senderJid.split('@')[0].split(':')[0];
+  const senderNumber = senderJid.split('@')[0].split(':')[0];
 
-return senderNumber === config.ownerNumber;
+  const ownerNumber = settingsStore.get('ownerNumber', config.ownerNumber);
+  return senderNumber === ownerNumber;
 }
 
 module.exports = { isOwner };
